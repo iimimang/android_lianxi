1749443d0016f733df3865775ad416e8d6b264e7
su <su@qq.com>
2014-12-18 09:31:43 +0800

    a

--- shangxiang/src/com/wyj/tabmenu/My.java
+++ shangxiang/src/com/wyj/tabmenu/My.java
@@ -1,7 +1,12 @@
 package com.wyj.tabmenu;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class My extends Activity
 {
@@ -10,5 +15,18 @@ public class My extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.my);
+		
+		ImageView login=(ImageView) findViewById(R.id.avatar);
+		login.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent();
+				intent.setClass(My.this, Login.class);
+				startActivity(intent); 
+			}
+		});	
+		
 	}
 }
